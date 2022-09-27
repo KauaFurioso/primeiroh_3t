60c1aee2cb8a03684daf3df784cb1dafdaee0695
kauã <106189667+KauaFurioso@users.noreply.github.com>
2022-09-27 16:51:03 -0300

    Add files via upload
    Aula 01

--- estudo.js
+++ estudo.js
@@ -1,1 +1,11 @@
+//alert("Oi, eu sou um alert!");
+//console.log("Oi, eu sou um console");
+//document.write("aula de pens. comp.");
+
+function chamaEu(){
+    alert("Oi, eu sou um alert!");
+}
+
+
+
 
